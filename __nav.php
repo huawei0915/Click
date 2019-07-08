14fe64fdb94e1f2ed273a08514904a7278bb4ff0
wii <pksos30066@gmail.com>
2019-07-08 21:14:31 +0800

    size

---
+++ __nav.php
@@ -0,0 +1,43 @@
+
+    <header>
+        <div class="container">
+            <div class="row justify-content-between align-items-end">
+                <a href=""><img src="img/logo.svg" alt=""></a>
+                <div class="features pb-2">
+                    <a href=""><img src="img/icon/會員.svg" alt="" class="px-2"></a>
+                    <a href=""><img src="img/icon/購物車.svg" alt="" class="px-2"></a>
+                    <a href=""><img src="img/icon/放大鏡.svg" alt="" class="px-2"></a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <nav>
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <ul class="d-flex nav-l">
+                    <li><a href="">首頁</a></li>
+                    <li><a href="">CANON</a></li>
+                    <li><a href="">NIKON</a></li>
+                    <li><a href="" style="letter-spacing:2px;">LEICA</a></li>
+                    <li><a href="">產品列表</a></li>
+                    <li><a href="">客製服務</a></li>
+                    <li><a href="">攝影分享</a></li>
+                </ul>
+                <ul class="d-flex nav-r">
+                    <li><a href="">關於我們</a></li>
+                    <li><a href="">客服資源</a></li>
+                    <li><a href="">FAQ</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+
+
+
+
+    
+  
