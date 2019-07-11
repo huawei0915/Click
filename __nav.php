ff07f2b794bab9f8708950a2f00f9f4bbb7f2c50
wii <pksos30066@gmail.com>
2019-07-11 18:24:36 +0800

    table

--- __nav.php
+++ __nav.php
@@ -1,12 +1,16 @@
 <body>
-    <header>
+<header>
         <div class="container">
             <div class="row justify-content-between align-items-end">
                 <div class="logo"><a href=""><img src="img/logo.svg"alt=""></a></div>
                 <div class="features pb-2">
+                        <form method="post" action="">
                         <div class="box">
-                                <input id="mag" type="text" placeholder="    Search...">
-                            <div id="arm"></div>
+                               
+                                <input id="mag" type="search" placeholder="    Search..." name="search">
+                             
+                                <div id="arm"></div>
+                            </form>
                     <!-- <input type="search" id="searcharea" name="searcharea" class="searcharea" value="搜尋"> -->
                     <!-- <input type="search"><img src="img/icon/放大鏡.svg" alt="" class="px-2" id="search"></a> -->
                     <a href=""><img src="img/icon/shop.svg" alt="" class="px-3 ml-2" style="height: 28px;margin-top: 5px;"></a>
@@ -24,7 +28,7 @@
                     <li><a href="" >NIKON</a></li>
                     <li><a href="" style="letter-spacing:1px;">LEICA</a></li>
                     <li><a href="" >產品列表</a></li>
-                    <li><a href="" >客製服務</a></li>
+                    <li><a href="" >首選推薦</a></li>
                     <li><a href="" >攝影分享</a></li>
                 </ul>
                 <ul class="d-flex nav-r">
