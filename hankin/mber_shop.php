ba539d279af87918696c47c65ebdeb8c9e4981c2
hankin076 <hank8202@gmail.com>
2019-07-16 17:11:37 +0800

    07-16

--- hankin/mber_shop.php
+++ hankin/mber_shop.php
@@ -1,25 +1,34 @@
 <?php include __DIR__ . '/__html_head.php' ?>
 <?php include __DIR__ . '/__nav.php' ?>
 
-<div class="container">
-    <div class="bg_mber">   
+<div class="container" style="padding:0px;">
+    <div class="bg_mber">  
         <div class="left_sec">
             <div class="circle_sec d-flex">
-            <div class="circle_ckh">
+                <div class="circle_ckh">
                 <img src="img/CKH.jpg" alt="">
-            </div>
-            <ul class="cirle_edit">
-                <li class="circle_name">CKH</li>
+                </div>
+                <ul class="cirle_edit">
+                 <li class="circle_name">CKH</li>
                 <li>
-                    <a href="">
-                    <i class="far fa-edit">
+                    <a href=""><i class="far fa-edit">
                     </i>編輯個人簡介</a>
                 </li>
-            </ul>
+                </ul>
+            </div>
+            <div class="left_list">
+                <ul>
+                    <li><a href=""><i class="fas fa-user"></i>我的帳戶</a></li>
+                    <li><a href=""><i class="far fa-list-alt"></i>購買清單</a></li>
+                    <li><a href=""><i class="fas fa-clipboard-list"></i>訂單查詢</a>  </li>
+                    <li><a href=""><i class="far fa-heart"></i>我的收藏</a></li>
+                </ul>
             </div>
         </div>
     </div>
 </div>
+
+
 
 
 
--- hankin/sql/members.sql
+++ hankin/sql/members.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2019 年 07 月 15 日 09:54
+-- 產生時間： 2019 年 07 月 16 日 08:54
 -- 伺服器版本： 10.3.16-MariaDB
 -- PHP 版本： 7.2.19
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫： `proj54`
+-- 資料庫： `member`
 --
 
 -- --------------------------------------------------------
@@ -29,27 +29,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `members` (
-  `id` int(11) NOT NULL,
+  `sid` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `password_check` varchar(255) NOT NULL,
   `mobile` varchar(255) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `sex` int(11) NOT NULL,
   `birthday` date DEFAULT NULL,
+  `address` int(11) DEFAULT NULL,
   `hash` varchar(255) NOT NULL,
   `activated` int(11) NOT NULL DEFAULT 0,
-  `nickname` varchar(255) NOT NULL,
   `create_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- 傾印資料表的資料 `members`
---
-
-INSERT INTO `members` (`id`, `email`, `password`, `mobile`, `address`, `birthday`, `hash`, `activated`, `nickname`, `create_at`) VALUES
-(3, 'ming@gg.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '0918222333', '', '1990-02-02', '52c61e86824899ca67e8d815a7a7afb0ce43878c', 0, '老明', '2019-01-07 10:39:38'),
-(4, 'aaa@qq.com', '3d4f2bf07dc1be38b20cd6e46949a1071f9d0e3d', '0988888888', '', '0000-00-00', '1bc88dddf4783874a5f50c8955b703be32775ca1', 0, 'aaa', '2019-01-10 10:29:53'),
-(5, 'aaa@qq.coma', '3d4f2bf07dc1be38b20cd6e46949a1071f9d0e3d', '0911111111', '', '0000-00-00', '93264d4396836844f43a4cb1669827ba41ff5d58', 0, 'aaaa', '2019-01-10 10:33:20'),
-(6, 'aaaaaaaa@qq.coma', '3d4f2bf07dc1be38b20cd6e46949a1071f9d0e3d', '0911111111', '', '0000-00-00', '57774e6b1c6ccc3913fd8be593836c384d5da1a3', 0, 'aaaa', '2019-01-10 10:34:27');
 
 --
 -- 已傾印資料表的索引
@@ -59,7 +51,7 @@ INSERT INTO `members` (`id`, `email`, `password`, `mobile`, `address`, `birthday
 -- 資料表索引 `members`
 --
 ALTER TABLE `members`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`sid`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
@@ -70,7 +62,7 @@ ALTER TABLE `members`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `members`
 --
 ALTER TABLE `members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
