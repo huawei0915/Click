6d020e980c2a89ecf700f0b52983afa1256f46d5
hankin076 <hank8202@gmail.com>
2019-07-17 21:34:04 +0800

    07-17

--- hankin/mber_shop.php
+++ hankin/mber_shop.php
@@ -8,30 +8,33 @@
                 <div class="circle_ckh">
                 <img src="img/CKH.jpg" alt="">
                 </div>
-                <ul class="cirle_edit">
+            <ul class="cirle_edit">
                  <li class="circle_name">CKH</li>
                 <li>
                     <a href=""><i class="far fa-edit">
                     </i>編輯個人簡介</a>
                 </li>
-                </ul>
+            </ul>
             </div>
             <div class="left_list">
                 <ul>
                     <li><a href=""><i class="fas fa-user"></i>我的帳戶</a></li>
                     <li><a href=""><i class="far fa-list-alt"></i>購買清單</a></li>
-                    <li><a href=""><i class="fas fa-clipboard-list"></i>訂單查詢</a>  </li>
+                    <li><a href=""><i class="fas fa-clipboard-list"></i>訂單查詢</a></li>
                     <li><a href=""><i class="far fa-heart"></i>我的收藏</a></li>
                 </ul>
             </div>
         </div>
+        <div class="right_sec">
+            <div class="row row_p">
+                <div class="col-3"><a href="">待付款</a></div>
+                <div class="col-3"><a href="">待出貨</a></div>
+                <div class="col-3"><a href="">待收貨</a></div>
+                <div class="col-3"><a href="">完成</a></div>
+            </div>
+        </div>
     </div>
 </div>
-
-
-
-
-
 <?php include __DIR__ . '/__footer.php' ?>
 <?php include __DIR__ . '/__script.php' ?>
 <?php include __DIR__ . '/__html_end.php' ?>
