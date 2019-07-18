69ef5c88e4bbe0ca322b55f80ef82979e1a3ede0
wii <pksos30066@gmail.com>
2019-07-18 20:57:16 +0800

    diy 圖片位置

--- diy.php
+++ diy.php
@@ -12,10 +12,13 @@
                             <li><a href="">進階<i class="fas fa-caret-right fa-sm"></i></a></li>
                             <li><a href="">入門<i class="fas fa-caret-right fa-sm"></i></a></li>
                     </ul>
-                    <div class="camera_BP">
+                    <div class="camera_BP" >
+                    <figure class="text-center">
+                            <img id="camera_BP" src="" alt="">
+                    </figure>
 
                     </div>
-                    <div class="camera_Intro">
+                    <div class="camera_Intro" >
 
 
                     </div>
@@ -24,27 +27,31 @@
 
 
                     </div>
-                    <div class="lens_BP">
+                    <div class="lens_BP" >
 
-
+                    <figure class="text-center">
+                            <img id="lens_BP" src="" alt="">
+                    </figure>
 
                     </div>
-                    <div class="tools1_BP">
-
+                    <div class="tools1_BP" >
+                    <figure class="text-center">
+                            <img id="tools1_BP" src="" alt="">
+                    </figure>
 
                     </div>
                     <div class="tools1_Intro">
 
                         
                     </div>
-                    <div class="tools2_BP">
+                    <!-- <div class="tools2_BP">
 
 
                     </div>
                     <div class="tools2_Intro">
 
                         
-                    </div>
+                    </div> -->
                     <div class="redo"><i class="fas fa-redo-alt"></i></div>
                     
                 </div>
@@ -76,10 +83,37 @@
         </div>
         <div class="arrow">
         <span><i class="fas fa-chevron-left fa-3x"></i></span>
+        <div class="d-flex justify-content-center thumbnail" id="thumbnail">
+            <figure>
+                <img src="./img/1.png" alt="">
+            </figure>
+            <figure>
+                <img src="./img/1.png" alt="">
+            </figure>
+            <figure>
+                <img src="./img/1.png" alt="">
+            </figure>
+            <figure>
+                <img src="./img/1.png" alt="">
+            </figure>
+        </div>
+
+
+
         <span><i class="fas fa-chevron-right fa-3x"></i></span>
         </div>
     </section>   
 
 <?php include __DIR__ . '/__footer.php' ?>
 <?php include __DIR__ . '/__script.php' ?>
+<script>
+            $("#thumbnail figure").click(function(){
+                let img=$(this).find("img").attr("src");
+                // console.log(picture);
+                $("#camera_BP").attr("src",img);
+                // ---------------------
+                $(this).addClass("active").siblings().removeClass("active");
+
+            });
+        </script>
 <?php include __DIR__ . '/__html_end.php' ?>
