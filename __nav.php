190b14fcef8a12685f15ec9c3baf1f9aeff6887b
wii <pksos30066@gmail.com>
2019-07-18 15:32:30 +0800

    漢堡鈕

--- __nav.php
+++ __nav.php
@@ -1,16 +1,35 @@
 <body>
-<header>
+    <header>
         <div class="container">
-            <div class="row justify-content-between align-items-end">
-                <div class="logo"><a href=""><img src="img/logo.svg"alt=""></a></div>
+            <div class="row justify-content-between align-items-end  mobile-style">
+                <div class="d-flex head_mobile">
+                    
+                    <div class="logo px-2"><a href=""><img src="img/logo.svg"alt=""></a></div>
+                </div>
                 <div class="features pb-2">
                         <form method="post" action="">
-                        <div class="serbox">
-                               <input id="mag" type="search" placeholder="    Search..." name="search">
+                            <div class="serbox">   
+                                <input id="mag" type="search" placeholder="    Search..." name="search">   
                                 <div id="arm"></div>
-                            </form>
-                    <a href=""><img src="img/icon/shop.svg" alt="" class="px-3 ml-2" style="height: 28px;margin-top: 5px;"></a>
+                        </form>
+                    <a href=""><img src="img/icon/shop.svg" alt="" class="px-3 ml-2 shop"></a>
                     <a href=""><img src="img/icon/會員.svg" alt="" class="px-2"></a>
+                    <div class="menu transition show_mobile " >
+                            <div class="bar bar1 transition"></div>
+                            <div class="bar bar2 transition"></div>
+                            <div class="bar bar3 transition"></div>
+                            <ul class="transition">
+                              <li><a class="transition">首頁</a></li>
+                              <li><a class="transition">CANON</a></li>
+                              <li><a class="transition">NIKON</a></li>
+                              <li><a class="transition">LEICA</a></li>
+                              <li><a class="transition">產品列表</a></li>
+                              <li><a class="transition">首選推薦</a></li>
+                              <li><a class="transition">攝影分享</a></li>
+                              <li><a class="transition">關於我們</a></li>
+                              <li><a class="transition">客服支援</a></li>
+                            </ul>
+                          </div>
                 </div>
             </div>
         </div>
@@ -20,9 +39,9 @@
             <div class="d-flex justify-content-between">
                 <ul class="d-flex nav-l">
                     <li><a href="" >首頁</a></li>
-                    <li><a href="" >CANON</a></li>
-                    <li><a href="" >NIKON</a></li>
-                    <li><a href="" style="letter-spacing:1px;">LEICA</a></li>
+                    <li><a href="" class="deskshow">CANON</a></li>
+                    <li><a href="" class="deskshow" >NIKON</a></li>
+                    <li><a href="" class="deskshow" style="letter-spacing:1px;">LEICA</a></li>
                     <li><a href="" >產品列表</a></li>
                     <li><a href="" >首選推薦</a></li>
                     <li><a href="" >攝影分享</a></li>
