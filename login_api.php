8366a7ed0f5d85bedf174a6b7ca5344a354e98d2
wii <pksos30066@gmail.com>
2019-07-19 21:08:31 +0800

    修改登入註冊 編輯的api
    登入註冊 編輯個人資料(不含更新密碼)

---
+++ login_api.php
@@ -0,0 +1,38 @@
+<?php
+require __DIR__. '/__db_connect.php';
+
+$result = [
+    'success' => false,
+    'code' => 0,
+    'info' => '資料欄位不足',
+    'post' => $_POST,
+];
+
+// TODO: 檢查欄位
+if(empty($_POST['email']) or empty($_POST['password'])){
+    // 兩個欄位只要有一個沒填, 就結束
+    echo json_encode($result);
+    exit;
+}
+
+
+$stmt = $pdo->prepare("SELECT `sid`, `email`, `nickname` 
+FROM `members`
+WHERE `email`=? AND `password`=SHA1(?)");
+
+$stmt->execute([
+    $_POST['email'],
+    $_POST['password'],
+]);
+
+if($stmt->rowCount()==1){
+    $result['success'] = true;
+    $result['info'] = '登入成功';
+
+    $_SESSION['loginUser'] = $stmt->fetch(PDO::FETCH_ASSOC);
+} else {
+    $result['info'] = '帳號或密碼錯誤';
+    $result['code'] = 400;
+}
+
+echo json_encode($result, JSON_UNESCAPED_UNICODE);
