2d949a009b2b7d1ac22427ee90a25bcc63256ef7
wii <pksos30066@gmail.com>
2019-07-31 12:42:38 +0800

    購物車完成

--- cart.php
+++ cart.php
@@ -44,7 +44,7 @@
                     <div id="step-1" class="step-1">
                         <div class="">
                             <h2 class=""><img src="img/icon/會員圈.svg" alt="">  個人資料(訂購人)</h2>
-                            <div class="wrapper">
+                            <div class="">
                                 <form class="text-right"　>
                                     <div class="form-group row ">
                                         <label for="name" class="col-sm-2 col-form-label ">姓名</label>
@@ -110,7 +110,7 @@
                                     <label class="custom-control-label people" for="customRadioInline0">同訂購人</label>
                                 </div>
                             </div>
-                            <div class="wrapper">
+                            <div class="">
                                 <form class="text-right"　>
                                     <div class="form-group row ">
                                         <label for="name" class="col-sm-2 col-form-label ">姓名</label>
@@ -262,96 +262,161 @@
                             </div>
                         <div class="total_price">
                             <div>
-                                <p>商品小計:</p>
-                                <p>運費:</p>
+                                <div class="d-flex sm_price">
+                                    <p class="text">商品小計:</p>
+                                    <p>NT$</p>
+                                    <p class="price">22222</p>
+                                </div>
+                                <div class="d-flex sm_send">
+                                    <p class="text">運費:</p>
+                                    <p>NT$</p>
+                                    <p class="price">0</p>
+                                </div>
                             </div>
                             <div>
-                                <p>總計:</p>
+                                <div class="d-flex sm_total">
+                                    <p class="text">總計:</p>
+                                    <p>NT$</p>
+                                    <p class="price"></p>
+                                </div>
                             </div>
                         </div>    
                     </div>
                     <div id="step-4" class="step-4">
                         <div class="" >
-                            <h2>物流方式</h2>
-                                <div class="wrapper delivery-main mt-2">
-                                    <div class="custom-control custom-radio custom-control-inline radio-text">
-                                        <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+                            <div class="d-flex title"><img src="img/icon/物流.svg" alt=""><h5 class="ml-3 pt-3">物流方式</h5></div>
+                                <div class=" delivery-main mt-2">
+                                    <div class="custom-control custom-radio  radio-text">
+                                        <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input" checked>
                                         <label class="custom-control-label" for="customRadioInline1">宅配</label>
                                     </div>
-                                    <div class="custom-control custom-radio custom-control-inline radio-text">
+                                    <div class="custom-control custom-radio  radio-text">
                                         <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input">
-                                        <label class="custom-control-label" for="customRadioInline2">自取 <em>(10:00am~19:00pm)</em></label>
+                                        <label class="custom-control-label " for="customRadioInline2">自取 <em>(10:00am~19:00pm)</em></label>
                                     </div>
-                                    <div class="custom-control custom-radio custom-control-inline radio-text">
+                                    <div class="custom-control custom-radio  radio-text">
                                         <input type="radio" id="customRadioInline3" name="customRadioInline1" class="custom-control-input">
                                         <label class="custom-control-label" for="customRadioInline3">超商取貨</label>
                                     </div>
                                 </div>
-                                <div class="wrapper delivery-main p-time mt-2">
-                                    <div class="p-time-title">配送時間 : </div>
-                                        <div class="p-time-area">
-                                            <div class="custom-control custom-radio custom-control-inline radio-text">
-                                                <input type="radio" id="customRadioInline4" name="customRadioInline2" class="custom-control-input">
-                                                <label class="custom-control-label" for="customRadioInline4">9:00~12:00</label>
-                                            </div>
-                                            <div class="custom-control custom-radio custom-control-inline radio-text">
-                                                <input type="radio" id="customRadioInline5" name="customRadioInline2" class="custom-control-input">
-                                                <label class="custom-control-label" for="customRadioInline5">14:00~17:00</label>
-                                            </div>
-                                            <div class="custom-control custom-radio custom-control-inline radio-text">
-                                                <input type="radio" id="customRadioInline6" name="customRadioInline2" class="custom-control-input">
-                                                <label class="custom-control-label" for="customRadioInline6">18:00~20:00</label>
+                                <div class=" delivery-main p-time mt-2 ">
+                                    <!-- ------------------------------------------------------------- -->
+                                    <div class="house" >
+                                        <div class="p-time-title">配送時間 : </div>
+                                            <div class="p-time-area">
+                                                <div class="custom-control custom-radio custom-control-inline radio-text">
+                                                    <input type="radio" id="customRadioInline4" name="customRadioInline2" class="custom-control-input">
+                                                    <label class="custom-control-label" for="customRadioInline4">9:00~12:00</label>
+                                                </div>
+                                                <div class="custom-control custom-radio custom-control-inline radio-text">
+                                                    <input type="radio" id="customRadioInline5" name="customRadioInline2" class="custom-control-input">
+                                                    <label class="custom-control-label" for="customRadioInline5">14:00~17:00</label>
+                                                </div>
+                                                <div class="custom-control custom-radio custom-control-inline radio-text">
+                                                    <input type="radio" id="customRadioInline6" name="customRadioInline2" class="custom-control-input">
+                                                    <label class="custom-control-label" for="customRadioInline6">18:00~20:00</label>
+                                                </div>
+                                                <div class="">  
+                                                    <em class="send_time">* (宅配需選擇配送時段)</em>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                <div class="wrapper delivery-main"><em>* (宅配需選擇配送時段)</em></div>
+                                        <!-- ------------------------------------------ -->
+                                        <div class="myself" >
+                                            <h5>聯絡資訊</h5>
+                                                <p>自取地址:台北市大安區和平東路106號3F<br>
+                                                    大安捷運站六號出口
+                                                </p>
+                                                <figure><img src="img/icon/地址.svg" alt=""></figure>
+                                            
+                                        </div>
+                                        <!-- ----------------------------------------- -->
+                                        <div class="family_711">
+                                            <h5>選取超商(門市)</h5>
+                                            <div class="d-flex w-100">
+                                                <select class="custom-select city" >
+                                                        <option selected>選擇縣市</option>
+                                                </select>
+                                                <select class="custom-select city_1">
+                                                        <option selected>鄉鎮地區</option>
+                                                </select>
+                                            </div>
+                                            
+                                            <select class="custom-select city_3">
+                                                    <option selected>選取門市</option>
+                                            </select>
+                                        </div>
+                                    </div>  
+                                </div>
+                                                         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                             <div class="my-4">
-                            <h2>付款方式</h2>
-                                <div class="pay-style d-flex ">
+                            <div class="d-flex title_1"><img src="img/icon/付款.svg" alt=""><h5 class="ml-3 pt-3">付款方式</h5></div>
+                                <div class="pay-style ">
                                     <div>
                                     <a href="" class="text-center visa" >
-                                        <p class="mt-3">信用卡</p>
-                                        <p>VISA<br>
-                                            asterCard<br>
-                                            JCB</p>
+                                        <p class="mt-3">信用卡<br>
+                                        VISA/MasterCard/JCB</p>
                                     </a>
                                     </div>
                                     <div>
                                     <a href="" class="text-center visa" >
-                                        <p class="mt-3">第三方支付</p>
-                                        <p>Line pay<br>
-                                            Google pay<br>
-                                            Apple pay</p>
+                                        <p class="mt-3">第三方支付<br>
+                                            Line pay/Google pay/Apple pay</p>
                                     </a>
                                     </div>
                                 </div>
                                 <div class="my-4 ">
-                                    <h3>發票類型</h3>
-                                    <div class="mt-2 pay-style">
-                                        <div class="custom-control custom-radio custom-control-inline radio-text">
-                                        <input type="radio" id="customRadioInline7" name="customRadioInline3" class="custom-control-input">
-                                        <label class="custom-control-label" for="customRadioInline7">二聯式 </label>
-                                    </div>
-                                    <div class="custom-control custom-radio custom-control-inline radio-text">
-                                        <input type="radio" id="customRadioInline8" name="customRadioInline3" class="custom-control-input">
-                                        <label class="custom-control-label" for="customRadioInline8">開立統編</label>
+                                    <div class="d-flex title_1"><img src="img/icon/發票.svg" alt=""><h5 class="ml-3 pt-3">發票類型</h5> </div>
+                                    <div class="mt-2 pay-style_1">
+                                        <div class="custom-control custom-radio  radio-text">
+                                            <input type="radio" id="customRadioInline7" name="customRadioInline3" class="custom-control-input" checked>
+                                            <label class="custom-control-label" for="customRadioInline7">二聯式 </label>
+                                        </div>
+                                        <div class="custom-control custom-radio  radio-text">
+                                        <div class="d-flex">
+                                            <input type="radio" id="customRadioInline8" name="customRadioInline3" class="custom-control-input">
+                                            
+                                            <label class="custom-control-label" for="customRadioInline8">開立統編</label>
+                                            <input type="text" class="number" placeholder="輸入統一編號">
+                                            </div>
                                     </div>
                                     <div class="mt-2 ">
-                                        <div class="custom-control custom-checkbox">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck1" checked>
-                                            <label class="custom-control-label" for="customCheck1">取件人與購買人相同</label>
-                                        </div>
-                                        <div class="custom-control custom-checkbox">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck2" checked>
-                                            <label class="custom-control-label" for="customCheck2">用上述資料直接註冊會員並同意會員責任規範及個資聲明</label>
-                                        </div>
+                                        
+                                        
                                     </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="d-flex justify-content-end mt-4"><a href="" class="btn btn-secondary mx-2 px-4">繼續購物</a><a href="" class="btn btn-danger mx-2 px-4">結   帳</a></div>
+                        <div class="d-flex justify-content-end mt-4">
+                            <a href="" class="btn btn-secondary mx-2 px-4">繼續購物</a>
+                            <a href="" class="btn btn-danger mx-2 px-4">結   帳</a>
+                        </div>
+
+                        <div class="custom-control custom-checkbox personal">
+                            <input type="checkbox" class="custom-control-input" id="customCheck2" checked>
+                            <label class="custom-control-label" for="customCheck2">用上述資料直接註冊會員並同意會員責任規範及個資聲明</label>
+                        </div>
                     </div>
+
+
+
+                                        
                     
                 </div>
             </div>
@@ -376,6 +441,26 @@
 //     }, 10);
 // }
 
+        $("#customRadioInline1").click(function(){
+            $(".house").show();
+            $(".myself").hide();
+            $(".family_711").hide();
+        })
+
+        $("#customRadioInline2").click(function(){
+            $(".house").hide();
+            $(".myself").show();
+            $(".family_711").hide();
+        })
+        $("#customRadioInline3").click(function(){
+            $(".house").hide();
+            $(".myself").hide();
+            $(".family_711").show();
+        })
+      
+        $("#customRadioInline8").click(function(){
+            $(".pay-style_1 .radio-text .number").css("display","block");
+        })
 
 </script>
 
