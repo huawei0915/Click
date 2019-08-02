3e9c9f32c2ef196bb5be0a9d2e15ce80765c1e7f
hankin076 <hank8202@gmail.com>
2019-08-02 20:54:27 +0800

    0801 init

--- hankin/mber_order.php
+++ hankin/mber_order.php
@@ -51,57 +51,72 @@
         </div>
         <!-- 右側區塊 -->
         <div class="my_order">
-            <div class="my_orderWord">
+            <div class="my_orderWord py-3">
                 訂單查詢 Order Tracking
-                <h6>訂單編號：000000</h6>
+                <h6 class="my_fh6 mx-3">訂單編號：000000</h6>
             </div>
-            <div class="order_line">
-                <img src="img/order_line.svg" alt="">
-            </div>
-            <div class="order_icon d-flex">
-                <img src="img/order_list.svg" alt="">
-                <img src="img/order_money.svg" alt="">
-                <img src="img/order_car.svg" alt="">
-                <img src="img/order_gps.svg" alt="">
-                <img src="img/order_done.svg" alt="">
+            <div class="ordr_icongroup">
+                <div class="order_line">
+                    <img src="img/order_line.svg" alt="">
+                </div>
+                <div class="order_icon d-flex">
+                    <img src="img/order_list.svg" alt="">
+                    <img src="img/order_money.svg" alt="">
+                    <img src="img/order_car.svg" alt="">
+                    <img src="img/order_gps.svg" alt="">
+                    <img src="img/order_done.svg" alt="">
+                </div>
             </div>
             <div class="order_userInfo">
                 <h4>收件人資訊</h4>
-                <div class="userinfo_user">
-                    <h6>收件人</h6>
-                    <label for="">張高華</label>
-                </div>
-                <div class="userinfo_mobile">
-                    <h6>電話</h6>
-                    <label for="">0987654321</label>
-                </div>
-                <div class="userinfo_address">
-                    <h6>地址</h6>
-                    <label for="">台北市XX區XX路XX巷XX弄XX號XX樓 </label>
+                <div class="userinfomation">
+                    <div class="userinfo_user d-flex">
+                        收件人
+                        <label for="">張高華</label>
+                    </div>
+                    <div class="userinfo_mobile d-flex">
+                        電話
+                        <label for="">0987654321</label>
+                    </div>
+                    <div class="userinfo_address d-flex">
+                        地址
+                        <label for="">台北市XX區XX路XX巷XX弄XX號XX樓 </label>
+                    </div>
                 </div>
             </div>
             <div class="order_listBuy">
                 <h4>訂單明細</h4>
-                <div class="order_buyList">
-                    <img src="" alt="">
+                <div class="order_buyList py-1">
+                    <figure>
+                        <img src="img/EOS5DMarkIV.png" alt="">
+                    </figure>
+                    <div class="order_infoBuy d-flex">
+                        <div class="order_name_spec">
+                            Canon EOS 5D Mark IV
+                            <br>
+                            EF 24-70mm f/4L IS USM
+                        </div>
+                        <div class="order_count_price">
+                            x 1
+                            <br>
+                            NT$ 169,000
+                        </div>
+                    </div>
                 </div>
-                <div class="order_name">Canon EOS 5D Mark IV</div>
-                <div class="order_spec">EF 24-70mm f/4L IS USM</div>
-                <div class="order_count">x 1</div>
-                <div class="order_price">NT $ 169,000</div>
             </div>
             <div class="order_payInfo">
+                <h4>付款資訊</h4>
                 <div class="order_send">
-                    <h6>寄送方式</h6>
+                    寄送方式
                     <label for="">宅配-NT $100</label>
                 </div>
                 <div class="order_date">
-                    <h6>訂單日期</h6>
+                    訂單日期
                     <label for="">2019-08-20</label>
                 </div>
                 <div class="odrder_total">
-                    <h6>購買總金額</h6>
-                    <label for="">NT $ 169,000</label>
+                    購買總金額
+                    <label for="">NT$ 169,000</label>
                 </div>
             </div>
         </div>
