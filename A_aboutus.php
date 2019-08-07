b38f3fa46906778a98aacaf68e5990ac192c6e46
Anthony-1980 <Anthony19800116@gmail.com>
2019-08-07 21:51:51 +0800

    Aboutus 2 zone finish

---
+++ A_aboutus.php
@@ -0,0 +1,251 @@
+<?php require __DIR__ . '/__html_head.php' ?>
+<?php require __DIR__ . '/__nav.php' ?>
+<style>
+  body {
+    overflow-y: scroll;
+    overflow-x: hidden;
+  }
+</style>
+<div class="A_aboutus_body">
+  <div class="A_aboutus_mbody">
+    <div class="A_aboutus_bg">
+      <span id="C">C</span>
+      <span id="L">L</span>
+      <span id="I">I</span>
+      <span id="C">C</span>
+      <span id="K">K</span>
+    </div>
+    <!-- <img src="img/test.jpg" alt=""> -->
+
+    <div class="A_us_main">
+      <h1 class="main-heading">
+        <span class="main-heading-primary">About Us</span>
+        <span class="main-heading-secondary">關於我們</span>
+      </h1>
+
+      <div class="A_us_sub">
+        <div class="row">
+          <a href="#" class="btn-item btn-effect">
+            Click的起點
+          </a>
+          <a href="#" class="btn-item btn-effect">
+            Click的堅持
+          </a>
+          <a href="#" class="btn-item btn-effect">
+            Click的合作
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="A_sub">
+    <div class="stars-wrapper" id="scene">
+      <div class="layer"></div>
+      <div class="stars-cluster stars-cluster-1 layer">
+        <div class="star star-1"></div>
+        <div class="star star-2"></div>
+        <div class="star star-3"></div>
+        <div class="star star-4"></div>
+        <div class="star star-5"></div>
+        <div class="star star-6"></div>
+        <div class="star star-7"></div>
+        <div class="star star-8"></div>
+        <div class="star star-9"></div>
+        <div class="star star-10"></div>
+        <div class="star star-11"></div>
+        <div class="star star-12"></div>
+        <div class="star star-13"></div>
+        <div class="star star-14"></div>
+        <div class="star star-15"></div>
+        <div class="star star-16"></div>
+        <div class="star star-17"></div>
+        <div class="star star-18"></div>
+        <div class="star star-19"></div>
+        <div class="star star-20"></div>
+        <div class="star star-21"></div>
+        <div class="star star-22"></div>
+        <div class="star star-23"></div>
+        <div class="star star-24"></div>
+        <div class="star star-25"></div>
+        <div class="star star-26"></div>
+        <div class="star star-27"></div>
+        <div class="star star-28"></div>
+        <div class="star star-29"></div>
+        <div class="star star-30"></div>
+        <div class="star star-31"></div>
+        <div class="star star-32"></div>
+        <div class="star star-33"></div>
+        <div class="star star-34"></div>
+        <div class="star star-35"></div>
+        <div class="star star-36"></div>
+        <div class="star star-37"></div>
+        <div class="star star-38"></div>
+        <div class="star star-39"></div>
+        <div class="star star-40"></div>
+      </div>
+      <div class="stars-cluster stars-cluster-2 layer">
+        <div class="star star-1"></div>
+        <div class="star star-2"></div>
+        <div class="star star-3"></div>
+        <div class="star star-4"></div>
+        <div class="star star-5"></div>
+        <div class="star star-6"></div>
+        <div class="star star-7"></div>
+        <div class="star star-8"></div>
+        <div class="star star-9"></div>
+        <div class="star star-10"></div>
+        <div class="star star-11"></div>
+        <div class="star star-12"></div>
+        <div class="star star-13"></div>
+        <div class="star star-14"></div>
+        <div class="star star-15"></div>
+        <div class="star star-16"></div>
+        <div class="star star-17"></div>
+        <div class="star star-18"></div>
+        <div class="star star-19"></div>
+        <div class="star star-20"></div>
+        <div class="star star-21"></div>
+        <div class="star star-22"></div>
+        <div class="star star-23"></div>
+        <div class="star star-24"></div>
+        <div class="star star-25"></div>
+        <div class="star star-26"></div>
+        <div class="star star-27"></div>
+        <div class="star star-28"></div>
+        <div class="star star-29"></div>
+        <div class="star star-30"></div>
+        <div class="star star-31"></div>
+        <div class="star star-32"></div>
+        <div class="star star-33"></div>
+        <div class="star star-34"></div>
+        <div class="star star-35"></div>
+        <div class="star star-36"></div>
+        <div class="star star-37"></div>
+        <div class="star star-38"></div>
+        <div class="star star-39"></div>
+        <div class="star star-40"></div>
+      </div>
+      <div class="stars-cluster stars-cluster-3 layer">
+        <div class="star star-1"></div>
+        <div class="star star-2"></div>
+        <div class="star star-3"></div>
+        <div class="star star-4"></div>
+        <div class="star star-5"></div>
+        <div class="star star-6"></div>
+        <div class="star star-7"></div>
+        <div class="star star-8"></div>
+        <div class="star star-9"></div>
+        <div class="star star-10"></div>
+        <div class="star star-11"></div>
+        <div class="star star-12"></div>
+        <div class="star star-13"></div>
+        <div class="star star-14"></div>
+        <div class="star star-15"></div>
+      </div>
+
+      <div class="title-wrapper layer"></div>
+    </div>
+  </div>
+
+  <!-- 2nd Zone Start -->
+
+  <div class="A_aboutus_2words">
+    <div class="A_aboutus2container">
+      <div class="cloud">
+
+        <h4 class="A_aboutus2container_h2"><br><br>CLICK的起點 The Start Of Click</h4><br>
+        <h2>每張照片, <br>背後都有個小小的期待。</h2><br>
+        <b>在那還是個叫做小叮噹的年代<br>
+          (現在中譯為多啦a夢)<br><br>
+
+          還記得第一次看到打開抽屉,<br>
+          搭上時光機回到過去前行未來的興奮心情嗎?<br><br>
+
+          當時電視機前的"葉大雄們",<br><br>
+
+          現在的你<br>
+          想搭上時光機去哪裡？<br>
+          未來？過去？<br><br>
+
+          會不會期待再見到生命中很重要<br>
+          但卻隨著日子一點一滴模糊掉的人、事、物？<br><br>
+
+          未來還在忙碌進行著<br>
+          在每個快門聲後即成過去留下的清晰<br><br>
+
+          這就是Click的起源<br><br></b>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- <div class="container article__container"> -->
+
+  <!-- <div class="animation-wrapper active">
+  <canvas id="animation-canvas"></canvas>
+  <canvas id="animation-space-canvas"> </canvas>
+</div> -->
+
+  <!-- <div class="row">
+		<div class="col-6">
+			<div class="article__content">
+				<div class="article__info">
+					<span class="article__collection">
+						CLICK的起點
+					</span>
+					<span class="article__date">
+						The Start Of Click
+					</span>
+				</div>
+				<h2 class="article__title">
+				每張照片, <br>背後都有個小小的期待。</h2>
+				<div class="article__author"> -->
+  <!-- <div class="left">
+					<img class="author__thumb" src="img/AS1.jpg" alt="">
+					</div> -->
+  <!-- <div class="right">
+						<div class="author__thumb-info">
+							<p class="author__name">
+							在那還是個叫做小叮噹的年代<br> 
+(現在中譯為多啦a夢)<br><br>
+
+還記得第一次看到打開抽屉,<br>
+搭上時光機回到過去前行未來的興奮心情嗎?<br><br>
+
+當時電視機前的"葉大雄們",<br><br>
+
+現在的你<br>
+想搭上時光機去哪裡？<br>
+未來？過去？<br><br>
+
+會不會期待再見到生命中很重要<br>
+但卻隨著日子一點一滴模糊掉的人、事、物？<br><br>
+
+未來還在忙碌進行著<br>
+在每個快門聲後即成過去留下的清晰<br><br>
+
+
+							</p>
+							<p class="author__title">
+							這就是Click的起源							</p>
+						</div>
+					</div>
+					 -->
+
+  <!-- </div>
+			</div>
+		</div>
+		<div class="col-6">
+			<img class="article__featured" src="img/AS3.jpg" alt="">
+		</div> -->
+  <!-- </div>
+	</div> -->
+  <!-- </div> -->
+</div>
+
+<!-- 2nd Zone End -->
+
+
+<?php require __DIR__ . '/__footer.php' ?>
+<?php require __DIR__ . '/__script.php' ?>
+<?php require __DIR__ . '/__html_end.php' ?>
