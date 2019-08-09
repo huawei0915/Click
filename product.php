8304579b4579f6ed98a3068dd52c8e4f93f850cf
CKH000 <starhua723@gmail.com>
2019-08-09 22:34:43 +0800

    單一產品頁

--- product.php
+++ product.php
@@ -7,7 +7,7 @@
     <div class="prdtop">
 
         <div class="d-flex">
-            <figure class="prdpic"><img src="" alt=""></figure>
+            <figure class="prdpic"><img src="img/product/camera/CC-01.png" alt=""></figure>
             <div class="prd_introduct">
                 <h1>EOS 5D Mark IV</h1>
                 <p>期待已久的EOS 5D Mark IV數位單眼相機搭載全新設計的3,040萬像素全片幅CMOS影像感測器及革命性Dual Pixel RAW，以追求更高影像品質；更配備承襲自旗艦型號EOS-1D X Mark II、全部61點自動對焦點均支援f/8光圈自動對焦的先進61點自動對焦系統及卓越的「雙像素CMOS自動對焦」技術，進一步提升攝影、錄影的自動對焦表現，同時支援DCI 4K短片拍攝，勢將拍攝錄影全面體驗進一步普及化。</p>
@@ -52,7 +52,90 @@
                     <li>內建GPS接收器及支援記錄器功能</li>
                 </ul>
             </div>
+        </div>  
+    </div>
 
+    <div class="prd_dtlspec">
+        <div class="dtlspec">
+        <h4>詳細規格</h4>
+            <img src="img/spec.png" alt="">
+        </div>
+    </div>
+
+    <div class="prd_characteristic">
+        <h4>產品特色</h4>
+        <div class="prd_charpic">
+            <img src="img/charpic.png" alt="">
+        </div>
+    </div>
+
+    <div class="prddown">
+        <div class="annex">
+            <h4>隨機附件</h4>
+            <div class="prd_annex d-flex">
+                <img src="img/canon_annex.jpg" alt="">
+                <ul>
+                    <li>EOS 5D Mark IV 數位相機</li>
+                    <li>電池LP-E6N</li>
+                    <li>充電器LC-E6E（附電源線）</li>
+                    <li>相機帶 EW-EOS5DMKIV</li>
+                    <li>界面連接線IFC-150U II（附連接線保護器）</li>
+                    <li>相機使用說明書及Wi-Fi（無線通訊）功能（基本）使用說明書</li>
+                    <li>CD-ROM (電子版繁體中文使用手冊)</li>
+                    <li>EOS DIGITAL Solution Disk（CD-ROM）</li>
+                    <li>中文入門指南</li>
+                    <li>產品保固卡</li>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="prd_text">
+            <h4>產品實測</h4>
+            <div class="text_cards d-flex">
+                <div class="tcs">
+                    <a href="img/text_big_pic1.jpg">
+                        <img src="img/prd_text_pic1.jpg" alt="">
+                    </a>
+                    <h6>大鬼湖台灣杉</h6>
+                    <p>攝影：雪羊snow ram</p>
+                    <ul>
+                        <li>EOS 6D</li>
+                        <li>EF 16-35mm f/2.8L II USM</li>
+                        <li>光圈 F8</li>
+                        <li>感光度 ISO 200</li>
+                        <li>快門 1/80 秒</li>
+                    </ul>
+                </div>
+                <div class="tcs">
+                    <a href="img/text_big_pic2.jpg">
+                        <img src="img/prd_text_pic2.jpg" alt="">
+                    </a>
+                    <h6>奇萊山屋日落火燒雲</h6>
+                    <p>攝影：雪羊snow ram</p>
+                    <ul>
+                        <li>EOS 5D Mark IV</li>
+                        <li>EF 50mm f/1.2L USM</li>
+                        <li>光圈 F13</li>
+                        <li>感光度 ISO 100</li>
+                        <li>快門 1/40 秒</li>
+                    </ul>
+                </div>
+                <div class="tcs">
+                    <a href="img/text_big_pic3.jpg">
+                        <img src="img/prd_text_pic3.jpg" alt="">
+                    </a>
+                    <h6>西稜奇峻山銀河</h6>
+                    <p>攝影：雪羊snow ram</p>
+                    <ul>
+                        <li>EOS 5D Mark IV</li>
+                        <li>EF 16-35mm f/2.8L II USM</li>
+                        <li>光圈 F2.8</li>
+                        <li>感光度 ISO 2000</li>
+                        <li>快門 15 秒</li>
+                    </ul>
+                </div>
+
+            </div>
         </div>
 
     </div>
