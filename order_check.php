b8190da71fe415089bb356b32bc8424aa6cf230f
hancheng076 <52851803+hancheng076@users.noreply.github.com>
2019-08-11 03:41:47 +0800

    init order_check finishing

---
+++ order_check.php
@@ -0,0 +1,74 @@
+<?php include __DIR__ . '/__html_head.php' ?>
+<?php include __DIR__ . '/__nav.php' ?>
+
+<div class="bg_mber">
+    <!-- -----order_check----- -->
+    <div class="container">
+        <div class="order_checkImg">
+            <img src="img/order_check.svg" alt="">
+        </div>
+    </div>
+    <!-- ----products_list----- -->
+    <div class="container">
+        <div class="order_checkProducts d-flex">
+            <div class="productsImg">
+                <img class="productsphoto" src="img/EOS5DMarkIV.png" alt="">
+            </div>
+            <div class="productsName">
+                EOS 5D Mark IV
+            </div>
+            <div class="productsCount">
+                x1
+            </div>
+            <div class="productsPrice">
+                NT $ 169,000
+            </div>
+        </div>
+    </div>
+    <div class="order_underline"></div>
+    <div class="container">
+        <div class="order_checkProducts d-flex">
+            <div class="productsImg">
+                <img class="productsphoto" src="img/EOS5DMarkIV.png" alt="">
+            </div>
+            <div class="productsName">
+                EOS 5D Mark IV
+            </div>
+            <div class="productsCount">
+                x1
+            </div>
+            <div class="productsPrice">
+                NT $ 169,000
+            </div>
+        </div>
+    </div>
+    <div class="order_underline"></div>
+    <div class="container">
+        <div class="order_checkProducts d-flex">
+            <div class="productsImg">
+                <img class="productsphoto" src="img/EOS5DMarkIV.png" alt="">
+            </div>
+            <div class="productsName">
+                EOS 5D Mark IV
+            </div>
+            <div class="productsCount">
+                x1
+            </div>
+            <div class="productsPrice">
+                NT $ 169,000
+            </div>
+        </div>
+    </div>
+    <div class="order_underline"></div>
+    <!-- -----BackHome & orderlist----- -->
+    <div class="container">
+        <div class="order_checkButton d-flex">
+            <a href="#" class="checkbutton_orderlist">訂單查詢</a>
+            <a href="#" class="checkbutton_backhome">回首頁</a>
+        </div>
+    </div>
+</div>
+
+<?php include __DIR__ . '/__footer.php' ?>
+<?php include __DIR__ . '/__script.php' ?>
+<?php include __DIR__ . '/__html_end.php' ?>
